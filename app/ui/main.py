a1b160d524063c075ef08234cbb20db8fbbdbdbb
max h <mharvy2000@gmail.com>
2020-03-24 13:48:00 +1000

    exported GUI over to kv file
    moved all gui elements and functions over to a kv file

--- app/ui/main.py
+++ app/ui/main.py
@@ -4,7 +4,6 @@ import kivy
 
 kivy.require('1.11.1')
 from kivy.config import Config
-
 Config.set('graphics', 'width', '640')
 Config.set('graphics', 'height', '960')
 Config.set('graphics', 'minimum_width', '360')
@@ -14,104 +13,58 @@ Config.set('widgets', 'scroll_moves', '10')
 
 from kivy.graphics import Color, Rectangle
 from kivymd.app import MDApp
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import NumericProperty, StringProperty, OptionProperty
 from kivymd.button import MDRaisedButton
 from kivymd.label import MDLabel
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivymd.textfields import MDTextField
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
-from functools import partial
+from kivymd.tabs import MDTabbedPanel, MDTab
+
+from datetime import datetime as DT
 import asyncio
 
 padding_def = 20
 
 
-class MainLayout(GridLayout):
-    debug_panel: BoxLayout
-    cmd_panel: BoxLayout
-
-    def __init__(self):
-        GridLayout.__init__(self)
-
-        self.cols = 1
-        self.padding = padding_def
-        self.spacing = padding_def / 2
-
-        self.debug_panel = DebugPanel()
-        self.cmd_panel = CmdPanel()
-
-        self.active_connection()
-        self.add_widget(self.debug_panel)
-        self.add_widget(self.cmd_panel)
-
-    def active_connection(self):
-        client = Client(auto_connect=False)
-        self.debug_panel.add_widget(MDLabel(
-            text=client.update()))
+class MainLayout(GridLayout): pass
 
 
-class DebugPanel(BoxLayout):
-    layout: GridLayout
-
+class Content(GridLayout):
     def __init__(self, **kwargs):
-        BoxLayout.__init__(self, **kwargs)
-        self.size = (Window.width, Window.height * 0.6)
-        layout = GridLayout(
-            cols=1, size_hint=(None, None),
-            width=self.width, height=self.height
-        )
-        layout.bind(minimum_height=layout.setter('height'))
-        # 50 rows of scrollable placeholder text
-        layout.add_widget(MDLabel(text=f"50 rows of text\n" * 25, size=self.size,
-                       size_hint=(None, None)))
-        layout.add_widget(MDLabel(text=f"50 rows of text\n" * 25, size=self.size,
-                                  size_hint=(None, None)))
+        GridLayout.__init__(self, **kwargs)
+        self.print_to_screen(
+            [str(f'[{x}] This is a line of text')
+             for x in range(50)])
 
-        root = ScrollView(size_hint=(None, None), size=self.size,
-                          do_scroll_x=False)
-        root.add_widget(layout)
-        self.add_widget(root)
+    def print_to_screen(self, lines: [str]):
+        for line in lines:
+            self.add_widget(MDLabel(
+                text=line
+            ))
+
+
+class DebugPanel(BoxLayout): pass
 
 
 class CmdPanel(BoxLayout):
-    def __init__(self, **kwargs):
-        BoxLayout.__init__(self, **kwargs)
-        self.orientation = 'vertical'
-        self.size_hint = (1, 0.4)
-        self.spacing = 10
-
-        cmd_input = TextInput(
-            size_hint_y=None,
-            height=self.height,
-            font_size=(Window.height / 42)
-        )
-
-        def send_cmd(instance):
-            client = Client(auto_connect=True)
-            client.send_cmd(cmd_input.text)
-
-        send_btn = MDRaisedButton(
-            text='send',
-            md_bg_color=(0.2, 0.6, 1, 1),
-            specific_text_color=[1, 1, 1, 0.87],
-            pos_hint={'right': 1},
-            on_press=send_cmd
-        )
-
-        self.add_widget(cmd_input)
-        self.add_widget(send_btn)
+    client_num = NumericProperty(1)
+    pass
 
 
-class MainApp(MDApp):
+class mainApp(MDApp):
+    padding_def = NumericProperty(20)
     title = "What should we call the program"
     icon = './icon/placeholder.jpg'
-
-    def build(self):
-        return MainLayout()
+    pass
 
 
 if __name__ == '__main__':
-    MainApp().run()
+    mainApp().run()
