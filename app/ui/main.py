03f9f825ca194ef5a6d6f6277952d6e197374f1c
max h <mharvy2000@gmail.com>
2020-03-28 17:40:39 +1000

    general changes

--- app/ui/main.py
+++ app/ui/main.py
@@ -8,10 +8,13 @@
 #  -    read from temporary log files
 
 import kivy
+from kivy.lang import Builder
+
 kivy.require('1.11.1')
 
 # kivy configuration
 from kivy.config import Config
+
 Config.set('graphics', 'width', '480')
 Config.set('graphics', 'height', '720')
 Config.set('graphics', 'minimum_width', '480')
@@ -22,6 +25,7 @@ Config.set('widgets', 'scroll_moves', '10')
 from app.transfer.android_client import Client
 from kivymd.app import MDApp
 from kivy.properties import NumericProperty, StringProperty, ListProperty
+from kivymd.button import MDRaisedButton
 from kivymd.label import MDLabel
 from kivy.clock import Clock
 import re
@@ -30,12 +34,14 @@ import time
 import asyncio
 import threading
 
-class mainApp(MDApp):
+
+class MainApp(MDApp):
     title = "What should we call the program"
     icon = './icon/app/app_icon1024x1024.jpg'
     padding_def = NumericProperty(20)
     status = StringProperty('')
     command = StringProperty('')
+    data = []
 
     def on_start(self):
         self.setup()
@@ -50,14 +56,23 @@ class mainApp(MDApp):
             self.status = client.get_connection
             log_msg = MDLabel(
                 text=self.status,
-                theme_text_color = 'Custom',
-                text_color = (1, 1, 1, 1)
+                theme_text_color='Custom',
+                text_color=(1, 1, 1, 1)
+            )
+            clear_btn = MDRaisedButton(
+                height=50,
+                text='clear',
+                pos_hint={'right': 1},
+                md_bg_color=(0.2, 0.6, 1, 1),
+                specific_text_color=[1, 1, 1, 0.87]
             )
             Clock.schedule_once(lambda x: self.root.ids['content_layout']
                                 .add_widget(log_msg))
+            Clock.schedule_once(lambda x: self.root.ids['bottom_panel']
+                                .add_widget(clear_btn))
         except:
             self.status = 'a problem occured,\nplease reload the app'
 
 
 if __name__ == '__main__':
-    mainApp().run()
+    MainApp().run()
