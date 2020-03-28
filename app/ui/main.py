e5e11ac2557aa48081e23453aa7df69073391192
max h <mharvy2000@gmail.com>
2020-03-28 13:29:48 +1000

    Added app icon folder

--- app/ui/main.py
+++ app/ui/main.py
@@ -1,14 +1,17 @@
-import re
-
-from kivy.uix.widget import Widget
-
-from app.transfer.android_client import Client
+# The main GUI of the application that handles all user
+# events, connected to the client.
+# Purposes:
+#  -    start/close application
+#  -    display debug log info
+#  -    receive/send user commands
+#  -    manually connect to client
+#  -    read from temporary log files
 
 import kivy
-
 kivy.require('1.11.1')
-from kivy.config import Config
 
+# kivy configuration
+from kivy.config import Config
 Config.set('graphics', 'width', '480')
 Config.set('graphics', 'height', '720')
 Config.set('graphics', 'minimum_width', '480')
@@ -16,52 +19,30 @@ Config.set('graphics', 'minimum_height', '720')
 Config.set('graphics', 'resizable', '1')
 Config.set('widgets', 'scroll_moves', '10')
 
-from kivy.graphics import Color, Rectangle
+from app.transfer.android_client import Client
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.app import App
-from kivy.lang import Builder
 from kivy.properties import NumericProperty, StringProperty, ListProperty
-from kivymd.button import MDRaisedButton
 from kivymd.label import MDLabel
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivymd.textfields import MDTextField
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.core.window import Window
-from kivymd.spinner import MDSpinner
-from kivymd.button import MDIconButton
 from kivy.clock import Clock
-
-import time
+import re
 from datetime import datetime as DT
+import time
 import asyncio
 import threading
 
-'''
-    self.print_to_screen(
-        [str(f'[{x}] This is a line of text')
-         for x in range(50)])
-
-    def print_to_screen(self, lines: [str]):
-        for line in lines:
-            self.add_widget(MDLabel(
-                text=line
-            ))
-'''
-
 class mainApp(MDApp):
     title = "What should we call the program"
-    icon = './icon/placeholder.jpg'
+    icon = './icon/app/app_icon1024x1024.jpg'
     padding_def = NumericProperty(20)
     status = StringProperty('')
     command = StringProperty('')
 
     def on_start(self):
         self.setup()
+
+    def send_command(self):
+        command = self.root.ids['cmd_input'].text
+        self.client.send_cmd(command)
 
     def setup(self):
         try:
@@ -76,12 +57,6 @@ class mainApp(MDApp):
                                 .add_widget(log_msg))
         except:
             self.status = 'a problem occured,\nplease reload the app'
-
-    def send_command(self):
-        msg = re.sub('[\\"]', '\'',
-              re.sub('[\\\\]', '/',
-              self.root.ids['cmd_input'].text))
-        self.client.send_cmd(msg)
 
 
 if __name__ == '__main__':
